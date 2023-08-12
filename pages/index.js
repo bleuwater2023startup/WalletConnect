c25b361904d707a1b5c5e127bf8fc563ee5e08dd
EECvision <eec.studies@gmail.com>
2023-08-12 12:02:30 +0100

    progress

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,38 @@
 import Head from "next/head";
 import { Web3Button } from "@web3modal/react";
+import { EthereumProvider } from "@walletconnect/ethereum-provider";
 
 export default function Home() {
+  const handleConnect = async () => {
+    try {
+      const provider = await EthereumProvider.init({
+        projectId: "4462f211ae9d8e31c96b53bd082c3ac0", // REQUIRED your projectId
+        chains: [1, 137], // REQUIRED chain ids
+        optionalChains: [5, 80001], // OPTIONAL chains
+        showQrModal: true, // REQUIRED set to "true" to use @walletconnect/modal
+        // qrModalOptions, // OPTIONAL - `undefined` by default, see https://docs.walletconnect.com/2.0/web3modal/options
+        methods: [
+          "eth_sendTransaction",
+          "eth_signTransaction",
+          "eth_sign",
+          "personal_sign",
+          "eth_signTypedData",
+        ],
+        chains: [80001],
+        events: ["chainChanged", "accountsChanged"],
+        rpcMap: {
+          80001: "https://rpc.ankr.com/polygon_mumbai",
+        },
+      });
+
+      console.log(provider);
+
+      await provider.enable();
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <div>
       <Head>
@@ -10,6 +41,8 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Web3Button />
+
+      <button onClick={handleConnect}>Connect</button>
     </div>
   );
 }
