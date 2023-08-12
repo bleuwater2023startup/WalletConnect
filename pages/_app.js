28fb39e4f7468e182fbb2830f74c0fe53e5712ca
EECvision <eec.studies@gmail.com>
2023-08-12 14:30:17 +0100

    remove walletButton

--- pages/_app.js
+++ pages/_app.js
@@ -1,32 +1,32 @@
 import "../styles/globals.css";
-import {
-  EthereumClient,
-  w3mConnectors,
-  w3mProvider,
-} from "@web3modal/ethereum";
-import { Web3Modal } from "@web3modal/react";
-import { configureChains, createConfig, WagmiConfig } from "wagmi";
-import { arbitrum, mainnet, polygon } from "wagmi/chains";
+// import {
+//   EthereumClient,
+//   w3mConnectors,
+//   w3mProvider,
+// } from "@web3modal/ethereum";
+// import { Web3Modal } from "@web3modal/react";
+// import { configureChains, createConfig, WagmiConfig } from "wagmi";
+// import { arbitrum, mainnet, polygon } from "wagmi/chains";
 
-const chains = [arbitrum, mainnet, polygon];
-const projectId = "4462f211ae9d8e31c96b53bd082c3ac0";
+// const chains = [arbitrum, mainnet, polygon];
+// const projectId = "4462f211ae9d8e31c96b53bd082c3ac0";
 
-const { publicClient } = configureChains(chains, [w3mProvider({ projectId })]);
-const wagmiConfig = createConfig({
-  autoConnect: true,
-  connectors: w3mConnectors({ projectId, chains }),
-  publicClient,
-});
-const ethereumClient = new EthereumClient(wagmiConfig, chains);
+// const { publicClient } = configureChains(chains, [w3mProvider({ projectId })]);
+// const wagmiConfig = createConfig({
+//   autoConnect: true,
+//   connectors: w3mConnectors({ projectId, chains }),
+//   publicClient,
+// });
+// const ethereumClient = new EthereumClient(wagmiConfig, chains);
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
-      <WagmiConfig config={wagmiConfig}>
-        <Component {...pageProps} />
-      </WagmiConfig>
+      {/* <WagmiConfig config={wagmiConfig}> */}
+      <Component {...pageProps} />
+      {/* </WagmiConfig> */}
 
-      <Web3Modal projectId={projectId} ethereumClient={ethereumClient} />
+      {/* <Web3Modal projectId={projectId} ethereumClient={ethereumClient} /> */}
     </>
   );
 }
