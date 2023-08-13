366b9fd4620a224da2beef01833d3f3254cc8fde
EECvision <eec.studies@gmail.com>
2023-08-13 02:47:15 +0100

    dafa

--- pages/index.js
+++ pages/index.js
@@ -1,36 +1,29 @@
 import Head from "next/head";
 // import { Web3Button } from "@web3modal/react";
 import { EthereumProvider } from "@walletconnect/ethereum-provider";
+import { useState } from "react";
 
 export default function Home() {
+  const [web3Provider, setProvider] = useState();
+
   const handleConnect = async () => {
+    console.log("connecting...");
     try {
       const provider = await EthereumProvider.init({
-        projectId: "4462f211ae9d8e31c96b53bd082c3ac0", // REQUIRED your projectId
-        chains: [1, 137], // REQUIRED chain ids
-        optionalChains: [5, 80001], // OPTIONAL chains
-        showQrModal: true, // REQUIRED set to "true" to use @walletconnect/modal
-        // qrModalOptions, // OPTIONAL - `undefined` by default, see https://docs.walletconnect.com/2.0/web3modal/options
-        methods: [
-          "eth_sendTransaction",
-          "eth_signTransaction",
-          "eth_sign",
-          "personal_sign",
-          "eth_signTypedData",
-        ],
-        chains: [80001],
-        events: ["chainChanged", "accountsChanged"],
-        rpcMap: {
-          80001: "https://rpc.ankr.com/polygon_mumbai",
-        },
+        projectId: "4462f211ae9d8e31c96b53bd082c3ac0",
+        chains: [1],
+        showQrModal: true,
       });
 
-      console.log(provider);
-
+      console.log("before:", provider);
       await provider.enable();
+      setProvider(provider);
+      console.log("after: ", provider);
     } catch (error) {
       console.log(error);
     }
+
+    console.log("connection ended");
   };
 
   return (
@@ -41,6 +34,17 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       {/* <Web3Button /> */}
+
+      <div>{web3Provider?.connected ? "connected" : "not connected"}</div>
+      <div>{web3Provider?.connecting ? "connecting" : "not connecting"}</div>
+      <div>
+        {web3Provider?.isWalletConnect
+          ? "isWalletConnect"
+          : "not isWalletConnect"}
+      </div>
+      <div>{web3Provider?.session ? "session" : "not session"}</div>
+      <div>{web3Provider?.accounts[0]}</div>
+      <div>{web3Provider?.chainId}</div>
 
       <button onClick={handleConnect}>Connect</button>
     </div>
